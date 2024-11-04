b8d91cbe9390d4cd1a1e7bcc36a70db01ec036f4
Malarselvi <156997996+Malarselvimarimuthu@users.noreply.github.com>
2024-11-04 22:31:25 +0530

    Fixed Suspense Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import authRoutes from './Router/auth-routes';
 // import nonAuthRoutes from './Router/non-auth-routes';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import SuspenseLayout from './Components/SuspenseLayout';
-import NavBar from './Components/Header';
+import SuspenseLayout from './Components/SuspenseLayout';
+import NavBar from './Components/NavigationLayout';
 function App() {
   return (
     <>
       <BrowserRouter>
-       <NavBar />
         <Routes>
-          {/* /* <Route  element= {<SuspenseLayout/>}> */}
-          {/* <Route element={<NavBar/>}> */}
-          <Route>
-            <Route>
+          <Route  element= {<SuspenseLayout/>}>
+            <Route element={<NavBar/>}>
               {authRoutes.navigationRouts.map((data) => {
                 return <Route path={data.path} key={data.name} element={data.component} />;
               })}
             </Route>
-            {/* </Route> */}
             {/* <Route>
               {nonAuthRoutes.map((data) => {
                 return <Route path={data.path} element={data.component} key={data.name} />;
